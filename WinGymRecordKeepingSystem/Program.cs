bb7e54f71c438396678c56040a72145aa59cc0b3
Whitehacker420 <cadetasharkhalid2@gmail.com>
2023-05-15 12:59:02 +0500

    admission screen

--- WinGymRecordKeepingSystem/Program.cs
+++ WinGymRecordKeepingSystem/Program.cs
@@ -17,7 +17,7 @@ namespace WinGymRecordKeepingSystem
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-           // Application.Run(new frmUsers());
+            // Application.Run(new frmUsers());
 
         }
     }
