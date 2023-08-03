122c7dbe3d6f3a132dea8ce4bb34964bed18875d
farazjahangir <jahangirfaraz98@gmail.com>
2023-08-03 11:59:46 +0500

    fix program.cs

--- WinGymRecordKeepingSystem/Program.cs
+++ WinGymRecordKeepingSystem/Program.cs
@@ -16,8 +16,8 @@ namespace WinGymRecordKeepingSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-           // Application.Run(new MDIParent());
-            Application.Run(new frmCollectFee());
+            Application.Run(new MDIParent());
+           // Application.Run(new frmCollectFee());
 
         }
     }
